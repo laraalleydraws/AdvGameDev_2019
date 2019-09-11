2334d5c7b77447e557f1d68c3a5ee17b3224e05f
laraalleydraws <laraalleydraws@gmail.com>
2019-09-11 15:05:25 -0400

    commit_3

---
+++ RevengeOfThePiggies/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform piggy;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goal = new Vector3(piggy.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, goal, Time.deltaTime * 5);
+    }
+}
+
--- RevengeOfThePiggies/Assets/CannonControl.cs
+++ RevengeOfThePiggies/Assets/CannonControl.cs
@@ -4,34 +4,48 @@ using UnityEngine;
 
 public class CannonControl : MonoBehaviour
 {
-    public GameObject piggyPlayer; // this is a reference to the player object
-    public float strenth = 200;
-    // Start is called before the first frame update
-    void Start()
-    {
-      
-    }
+	public Rigidbody2D piggyRB; // this is a reference to the player object
+	public Camera mainCamera;
+	Vector3 direction;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
-        Vector3 mousePositionInWorldCoordinates = Camera.main.ScreenToWorldPoint(mousePosition);
-        Vector3 direction = mousePositionInWorldCoordinates - transform.position;
+	const float STRENTH = 50; // the scalar that defines the STRENTH of the cannon
+	const int MAX_ANGLE = 80;
+	const int MIN_ANGLE = 5;
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
 
-        float alpha = Mathf.Acos(Vector3.Dot(Vector3.right, direction.normalized))*Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, alpha));
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x,
+			Input.mousePosition.y, -mainCamera.transform.position.z);
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            piggyPlayer.transform.parent = null;
-            piggyPlayer.GetComponent<Rigidbody2D>().gravityScale = 1;
-            piggyPlayer.GetComponent<Rigidbody2D>().AddForce(direction.normalized * strenth);
-        }
+		Vector3 mousePositionInWorldCoordinates = mainCamera.ScreenToWorldPoint(mousePosition);
+		direction = mousePositionInWorldCoordinates - transform.position;
 
-    }
-    public void Run()
-    {
+		float alpha = Mathf.Acos(Vector3.Dot(Vector3.right, direction.normalized)) * Mathf.Rad2Deg;
 
-    }
+		if (alpha < MAX_ANGLE && alpha > MIN_ANGLE && direction.y > 0)
+		{
+			transform.rotation = Quaternion.Euler(new Vector3(0, 0, alpha));
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetButtonUp("Fire1"))
+		{
+			piggyRB.transform.parent = null;
+			piggyRB.gravityScale = 1;
+			piggyRB.AddForce(direction * STRENTH * piggyRB.mass);
+		}
+	}
+
+	public void Run()
+	{
+
+	}
 }
