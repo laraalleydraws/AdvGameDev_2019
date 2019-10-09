256e60b4efc3bfd2bc344ed09f5f5deaaecb6e2c
laraalleydraws <laraalleydraws@gmail.com>
2019-10-09 14:02:00 -0400

    perlin

---
+++ PerlinNoise/Assets/CustomTerrain.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour
+{
+
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Texture2D heightMapImage;
+    public Vector3 heightMapScale = new Vector3(1, 1, 1);
+
+    public bool resetTerrain = true;
+
+    //PERLIN NOISE 
+    public float perlinXScale = 0.01f;
+    public float perlinYScale = 0.01f;
+    public int perlinOffsetX = 0;
+    public int perlinOffsetY = 0;
+    public int perlinOctaves = 3;
+    public float perlinPersistance = 8;
+    public float perlinHeightScale = 0.09f;
+
+
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    float[,] GetHeightMap()
+    {
+        if (!resetTerrain)
+        {
+            return terrainData.GetHeights(0, 0, terrainData.heightmapWidth,
+                                                terrainData.heightmapHeight);
+        }
+        else
+            return new float[terrainData.heightmapWidth,
+                             terrainData.heightmapHeight];
+
+    }
+
+    public void Perlin()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int y = 0; y < terrainData.heightmapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.heightmapWidth; x++)
+            {
+                heightMap[x, y] += Utils.fBM((x + perlinOffsetX) * perlinXScale,
+                                            (y + perlinOffsetY) * perlinYScale,
+                                            perlinOctaves,
+                                            perlinPersistance) * perlinHeightScale;
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+}
---
+++ PerlinNoise/Assets/PerlinNoise.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture ()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+     Color CalculateColor (int x, int y)
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / height * scale;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+    // Update is called once per frame
+    
+}
